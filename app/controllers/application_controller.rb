3ec5ecc5899a473b317b0c8d8973a710dd91b0d9
root <root@yoshihiro.>
2024-10-29 11:35:11 +0900

    23

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,9 +9,7 @@ class ApplicationController < ActionController::Base
  
 
   # ログイン後のリダイレクト先を設定
-  def after_sign_in_path_for(resource)
-    mypage_path  # マイページにリダイレクト
-  end
+ 
 
   # ログアウト後のリダイレクト先を設定
   def after_sign_out_path_for(resource_or_scope)
