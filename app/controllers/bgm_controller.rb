9fc41e04e81033191de71b842100e91fa962c50d
root <root@yoshihiro.>
2024-10-31 16:39:20 +0900

    34232

--- app/controllers/bgm_controller.rb
+++ app/controllers/bgm_controller.rb
@@ -1,4 +1,55 @@
+# app/controllers/bgm_controller.rb
+
+require 'net/http'
+require 'json'
+
 class BgmController < ApplicationController
   def index
+    # アクセストークンを取得
+    token = fetch_spotify_token
+    if token
+      # "ワークアウト"プレイリストをランダムに取得
+      @playlist = fetch_workout_playlist(token)
+    else
+      flash[:alert] = "Spotify APIへの接続に失敗しました。"
+    end
+  end
+
+  private
+
+  def fetch_spotify_token
+    url = URI("https://accounts.spotify.com/api/token")
+    request = Net::HTTP::Post.new(url)
+    request.basic_auth(Rails.application.credentials.spotify[:client_id], Rails.application.credentials.spotify[:client_secret])
+    request.set_form_data({
+      'grant_type' => 'client_credentials'
+    })
+
+    response = Net::HTTP.start(url.hostname, url.port, use_ssl: true) do |http|
+      http.request(request)
+    end
+
+    if response.is_a?(Net::HTTPSuccess)
+      JSON.parse(response.body)["access_token"]
+    else
+      nil
+    end
+  end
+
+  def fetch_workout_playlist(token)
+    url = URI("https://api.spotify.com/v1/browse/categories/workout/playlists?country=JP&limit=10")
+    request = Net::HTTP::Get.new(url)
+    request["Authorization"] = "Bearer #{token}"
+
+    response = Net::HTTP.start(url.hostname, url.port, use_ssl: true) do |http|
+      http.request(request)
+    end
+
+    if response.is_a?(Net::HTTPSuccess)
+      playlists = JSON.parse(response.body)["playlists"]["items"]
+      playlists.sample # ランダムに1つ選ぶ
+    else
+      nil
+    end
   end
 end
--- app/controllers/training_suggestions_controller.rb
+++ app/controllers/training_suggestions_controller.rb
@@ -1,31 +1,38 @@
 class TrainingSuggestionsController < ApplicationController
-    before_action :authenticate_user!
+  before_action :authenticate_user!
+  before_action :set_body_parts, only: [:new, :create]
+  def new
+    @body_parts = BodyPart.all  # 部位の選択肢を取得
+  end
+
+  def create
+    selected_part = params[:body_part_id]
+    part_name = BodyPart.find(selected_part).name
   
-    def new
-      @body_parts = BodyPart.all  # 部位の選択肢を取得
-    end
+    suggestions = {
+      '胸' => 'ベンチプレスを行いましょう！',
+      '背中' => '懸垂をやってみましょう！',
+      '脚' => 'スクワットを追加しましょう！',
+      '肩' => 'ショルダープレスで肩を鍛えましょう！',
+      '腕' => 'ダンベルカールで腕を強化しましょう！'
+    }
   
-    def create
-      selected_part = params[:body_part_id]
-      part_name = BodyPart.find(selected_part).name
+    suggestion = suggestions[part_name]
   
-      # 部位ごとの提案内容を決定
-      suggestions = {
-        '胸' => 'ベンチプレスを行いましょう！',
-        '背中' => '懸垂をやってみましょう！',
-        '脚' => 'スクワットを追加しましょう！',
-        '肩' => 'ショルダープレスで肩を鍛えましょう！',
-        '腕' => 'ダンベルカールで腕を強化しましょう！'
-      }
-  
-      suggestion = suggestions[part_name]
-  
-      # 提案結果を表示する
-      redirect_to training_suggestions_path(suggestion: suggestion)
-    end
-  
-    def index
-      @suggestion = params[:suggestion]  # 提案結果を表示
-    end
+    # `flash`でメッセージを一時的に保存し、`index`アクションにリダイレクト
+    flash[:suggestion] = suggestion
+    redirect_to training_suggestions_path
   end
   
+  
+
+  def index
+    # renderで遷移するため、ここには@suggestionを渡さない
+  end
+
+  private
+
+  def set_body_parts
+    @body_parts = BodyPart.all  # 部位の選択肢を取得
+  end
+end
