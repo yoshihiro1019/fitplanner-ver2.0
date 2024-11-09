7ac6a15428976ae0cfe09c03d6a243f1236cb9e6
root <root@yoshihiro.>
2024-11-09 14:29:07 +0900

    232

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,5 +1,5 @@
 
-class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
     @user = User.from_omniauth(request.env['omniauth.auth'])
 
