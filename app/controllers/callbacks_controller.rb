8bb5c9b2c44b52b51d6e6fb14e45a4718dd65fbe
root <root@yoshihiro.>
2024-12-23 22:09:35 +0900

    CI修正2

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -11,23 +11,23 @@ class CallbacksController < ApplicationController
 
       # アクセストークンを取得
       token_response = RestClient.post("https://accounts.spotify.com/api/token", {
-        grant_type: 'authorization_code',
+        grant_type: "authorization_code",
         code: code,
-        redirect_uri: 'http://localhost:3000/callback', # リダイレクトURI
+        redirect_uri: "http://localhost:3000/callback", # リダイレクトURI
         client_id: client_id,
         client_secret: client_secret
       })
-  
+
       token_info = JSON.parse(token_response.body)
-      access_token = token_info['access_token']
-  
+      access_token = token_info["access_token"]
+
       # アクセストークンを使ってユーザー情報を取得
-      user_response = RestClient.get('https://api.spotify.com/v1/me', {
+      user_response = RestClient.get("https://api.spotify.com/v1/me", {
         Authorization: "Bearer #{access_token}"
       })
-  
+
       user_info = JSON.parse(user_response.body)
-  
+
       # ユーザー情報を表示
       render json: user_info
     rescue RestClient::ExceptionWithResponse => e
@@ -36,4 +36,3 @@ class CallbacksController < ApplicationController
       render json: error_info, status: e.response.code
     end
   end
-  
