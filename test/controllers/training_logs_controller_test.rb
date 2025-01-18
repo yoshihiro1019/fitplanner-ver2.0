2b01329d3c85114321a6f8c97adc228061dce5b3
root <root@yoshihiro.>
2025-01-18 15:36:33 +0900

    test修正3

--- test/controllers/training_logs_controller_test.rb
+++ test/controllers/training_logs_controller_test.rb
@@ -1,6 +1,14 @@
 require "test_helper"
 
 class TrainingLogsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    # 例: FactoryBot でユーザーを作成
+    @user = FactoryBot.create(:user)
+
+    # Devise のヘルパーでログイン状態にする
+    sign_in @user
+  end
+
   test "should get index" do
     get training_logs_path
     assert_response :success
