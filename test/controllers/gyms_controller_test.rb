7631da60543e4af7c349b99988fa9fcd6d076ac0
root <root@yoshihiro.>
2025-01-18 14:46:18 +0900

    fixtures修正3

--- test/controllers/gyms_controller_test.rb
+++ test/controllers/gyms_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class GymsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get gyms_index_url
+    get gyms_index_path
     assert_response :success
   end
 end
