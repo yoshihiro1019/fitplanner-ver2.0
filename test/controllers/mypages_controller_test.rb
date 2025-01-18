0ad10bebf80d2d05b670bc83f4ef88463a946623
root <root@yoshihiro.>
2025-01-18 15:28:47 +0900

    test修正2

--- test/controllers/mypages_controller_test.rb
+++ test/controllers/mypages_controller_test.rb
@@ -1,8 +1,16 @@
 require "test_helper"
 
 class MypagesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    # テスト用のユーザを作成（FactoryBot がなければ fixtures などでもOK）
+    @user = FactoryBot.create(:user)
+    # Devise のヘルパーメソッドでログイン
+    sign_in @user
+  end
+
   test "should get show" do
     get mypage_path
     assert_response :success
   end
 end
+
