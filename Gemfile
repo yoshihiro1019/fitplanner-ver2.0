21bfb837c8d99c84ce8811cebf3a73b204bee9ee
root <root@yoshihiro.>
2025-01-18 21:18:21 +0900

    test修正5

--- Gemfile
+++ Gemfile
@@ -66,6 +66,7 @@ group :development do
 end
 
 group :test do
+  gem 'webmock'
   # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
   gem "capybara"
   gem "selenium-webdriver"
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,19 +1,55 @@
+# test/test_helper.rb
+
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
+# --- (1) WebMockの読み込み & 設定 ---
+require "webmock/minitest"      # Minitest で WebMock を使うため
+WebMock.disable_net_connect!(
+  allow_localhost: true         # ローカルのテストサーバ(例: Selenium/Capybara)を許可
+)
+
+# 必要に応じて、ここでAPIのスタブを定義しておく
+# 例: Spotify APIをモックする
+stub_request(:post, "https://accounts.spotify.com/api/token").to_return(
+  status: 200,
+  body: { access_token: "dummy_token_for_tests" }.to_json,
+  headers: { "Content-Type" => "application/json" }
+)
+
+stub_request(:get, "https://api.spotify.com/v1/browse/new-releases?limit=10").to_return(
+  status: 200,
+  body: {
+    albums: {
+      items: [
+        { "name" => "Mock Album 1" },
+        { "name" => "Mock Album 2" }
+      ]
+    }
+  }.to_json,
+  headers: { "Content-Type" => "application/json" }
+)
+
 module ActiveSupport
   class TestCase
-    # Run tests in parallel with specified workers
+    # --- (2) 並列テスト設定 ---
     parallelize(workers: :number_of_processors)
-
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+    
+    # --- (3) fixtures を読み込む場合 ---
     fixtures :all
-
-    # Add more helper methods to be used by all tests here...
+    
+    # ここに共通で使うヘルパーメソッドを定義してもよい
   end
 end
 
+# --- (4) DeviseのIntegrationHelpersを読み込む ---
 class ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 end
+
+# --- (5) (任意) Capybara/SystemTest設定 ---
+# require "capybara/rails"
+# class ActionDispatch::SystemTestCase
+#   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+# end
