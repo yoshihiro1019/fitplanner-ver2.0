715bbe8fcc338dbe38064315ff3eb358999a4247
root <root@yoshihiro.>
2025-01-22 22:30:39 +0900

    絵文字導入

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,17 @@ module.exports = {
     './app/javascript/**/*.js',
   ],
   theme: {
-    extend: {colors: {
-      'red-200': '#FECACA', // 明るい赤
-      'red-400': '#FCA5A5', // 少し濃い赤
-      'red-600': '#DC2626', // 中程度の赤
-      'red-800': '#9B1C1C', // 濃い赤
-    },},
+    extend: {
+      colors: {
+        'red-200': '#FECACA', // 明るい赤
+        'red-400': '#FCA5A5', // 少し濃い赤
+        'red-600': '#DC2626', // 中程度の赤
+        'red-800': '#9B1C1C', // 濃い赤
+      },
+      fontFamily: {
+        emoji: ['system-ui', '-apple-system', 'Segoe UI Emoji', 'Apple Color Emoji', 'Noto Color Emoji', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
